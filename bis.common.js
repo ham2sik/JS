51b29fc8ef26b6a5e2866902571ea310d630e1ef
ham2sik <ham2sik@gmail.com>
2016-10-11 09:38:26 +0900

    Update bis.common.js

--- bis.common.js
+++ bis.common.js
@@ -6,6 +6,28 @@ window.log = function(){
   }
 };
 
+/* parameter fn - ex)QueryString.listNo */
+var QueryString = function () {
+	var query_string = {},
+		query = window.location.search.substring(1);
+		vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		// If first entry with this name
+		if (typeof query_string[pair[0]] === "undefined") {
+			query_string[pair[0]] = decodeURIComponent(pair[1]);
+		// If second entry with this name
+		} else if (typeof query_string[pair[0]] === "string") {
+			var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+			query_string[pair[0]] = arr;
+		// If third or later entry with this name
+		} else {
+			query_string[pair[0]].push(decodeURIComponent(pair[1]));
+		}
+	}
+	return query_string;
+}();
+
 var listHeightResize = function($elem) { // var $list = $('#partSearch .rstList'); listHeightResize($('#partSearch .rstList'));
 	var $li=$elem.find('li'),
 		liSize=$li.size(),
